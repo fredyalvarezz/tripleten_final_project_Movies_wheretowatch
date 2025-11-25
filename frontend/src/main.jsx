0093875506c0580990e9b10b43ddd0f80ddec528
fredyalvarezz <68304047+fredyalvarezz@users.noreply.github.com>
2025-11-25 10:05:44 -0700

    Revert "1.2 completo Frontend funcional"

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './components/App/App.jsx';
-import "./vendor/normalize.css";
-import "./vendor/fonts.css"
 import './index.css'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
