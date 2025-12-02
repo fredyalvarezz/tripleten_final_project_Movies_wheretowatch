deefbc0380e919a5d420c4106d0d78a44f084891
Fredy <fredy-alexander1@hotmail.com>
2025-12-01 23:30:13 -0700

    actualizacion modificacion de validate en watchlist

--- backend/routes/watchlist.js
+++ backend/routes/watchlist.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { getItems, createItem, deleteItem } = require("../controllers/watchlist");
+const { validateId } = require("../middlewares/validate");
 
 router.get("/", getItems);
 router.post("/", createItem);
